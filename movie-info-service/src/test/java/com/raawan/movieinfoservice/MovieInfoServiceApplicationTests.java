d7539ac1e8d12c176859ca9396feef13b31297fc
Tejas8079 <tejas.chavan8079@gmail.com>
2022-02-26 23:38:20 +0530

    Microservices demo init

---
+++ movie-info-service/src/test/java/com/raawan/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raawan.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
